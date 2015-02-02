9a31c6f584974a03952990226798c1983c5ee847
frol <frolvlad@gmail.com>
2015-02-02 17:07:40 +0200

    Fixed user model reference as Django 1.7 has changed application load process

--- netauth/models.py
+++ netauth/models.py
@@ -1,14 +1,10 @@
+from django.conf import settings
 from django.db import models
 
-try:
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-except:
-    from django.contrib.auth.models import User
-
+AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
 
 class NetID(models.Model):
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(AUTH_USER_MODEL)
     identity = models.CharField(u'Net ID', max_length=255, unique=True)
     provider = models.CharField(u'Net authentication provider name', max_length=255)
 
